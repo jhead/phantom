910ee7ac7795e70ce7d29d95b7b21195100c9d7f
studioeng <byattsystems+github@gmail.com>
2019-09-02 21:52:39 +0100

    Issue-#5: Updates ProxyServer properties

--- internal/proxy/proxy.go
+++ internal/proxy/proxy.go
@@ -27,6 +27,9 @@ type ProxyServer struct {
 	idleMap             idleMap
 	pingChannel         chan PingPacket
 	dead                bool
+	version				string
+	name				string
+	users				int
 }
 
 type PingPacket struct {
