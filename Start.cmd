26ec6088141e8f8d331813b52535f1be442b1ba5
Steven Tracey <70534545+nevetS-718@users.noreply.github.com>
2021-04-09 09:41:03 -0400

    Finished

--- Start.cmd
+++ Start.cmd
@@ -1,7 +1,14 @@
 @echo off
 title Console Connector
 color f0
-set /p ip="Enter the server IP you wish to connect to"
-set /p port="Enter the server PORT you wish to connect to"
 
-./phantom-<os> -server lax.mcbr.cubed.host:19132
+echo Enter the server IP you wish to connect to
+set /p ip=
+
+echo Enter the server PORT you wish to connect to
+set /p port=
+
+echo Attempting to Lauch Connection...
+./phantom-Windows -server %ip%:%port%
+echo success!
+timeout /t 3 >nul
